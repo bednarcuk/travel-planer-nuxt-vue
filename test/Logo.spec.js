00d8c272382c402ff6fbe8a8a0a39440c2e2fbcf
Leon Bednarcuk <leon.bednarcuk@frontech.ca>
2019-12-16 00:53:41 -0700

    components test utils

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,5 +1,8 @@
+import Vue from 'vue'
 import { mount } from '@vue/test-utils'
 import Logo from '@/components/Logo.vue'
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
